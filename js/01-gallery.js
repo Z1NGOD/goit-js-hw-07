e959d0257117537bcd9e02cd172f6fc423664cd1
Z1NG0D <borovskiyyaroslavv@gmail.com>
2023-05-27 18:59:33 +0200

    comit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+gallery.addEventListener('click', onGalleryClick);
+function onGalleryClick(event) {
+    event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`
+  );
+  instance.show();
+}
+function createGalleryMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+        </li>`;
+        }).join('');
+}
+
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+const galleryMarkup =
+(galleryItems)=> {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+        </li>`;
+        }).join('');
+}
+gallery.insertAdjacentHTML('beforeend', galleryMarkup(galleryItems));
+
+gallery.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    }
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    animationSpeed: 250,
+});
+    
+
 console.log(galleryItems);
